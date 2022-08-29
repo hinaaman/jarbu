31cd7d6d4e4f22beec7f8b66ed9fa125f358bc5a
Hina Aman <hina.aman@gmail.com>
2022-08-28 20:11:52 -0700

    Update react script version

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = ['a'];
+  a?.forEach(a => console.log(a))
   return (
     <div className="App">
       <header className="App-header">
